a23e1b4dc3475fece31cfa6213c26f7e910c23af
Dean <deansingleton1999@gmail.com>
2020-06-17 15:48:36 +0100

    replaced grep with sed
    greps -P option was removed

--- fetch_gmail.sh
+++ fetch_gmail.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-emailCount=$(curl -u email:Password --silent "https://mail.google.com/mail/feed/atom" | grep -o -P '(?<=<fullcount>).*(?=</fullcount>)')
+atom=$(curl -u email:password --silent "https://mail.google.com/mail/feed/atom")
 
-	if [[ $? == 0 ]]; then
-		if [[ $emailCount = 0 ]]
-		then
-        		:
-    		else
-        		export DISPLAY=:0
-        		notify-send -i /usr/share/icons/gnome/16x16/status/mail-unread.png "New E-mail" "You have $emailCount unread E-mails"
-    		fi
-         else
-		:
-	fi
+emailCount=$(echo "$atom" | sed 's/\(.*\)fullcount>\(.*\)<\/fullcount>\(.*\)/\2/g')
 
 
+echo "$emailCount"
 
+if [[ $? == 0 ]]; then
+        if [[ $emailCount = 0 ]]; then
+                :
+        else
+                export DISPLAY=:0
+                notify-send -i /usr/share/icons/gnome/16x16/status/mail-unread.png "New E-mail" "You have $emailCount unread E-mails"
+        fi
+else
+        :
+fi
